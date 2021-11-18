dd41ab9412985c25c1a5759ec9beec5fdd3508da
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2021-11-18 15:05:22 -0300

    construção da tarefa criação do quizz: Perguntas do quizz, incompleta

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+let quantidadeDePerguntas = 0;
+let contadorPergunta = 1; 
+
 //quizzesLocais()
+teste();
+function teste (){
+    const teste = document.querySelectorAll(".respostasIncorretas");
+    console.log(teste);
+}
 
 function telaDesaparece(){
     const quizzDesaparece = document.querySelector(".quizz")
@@ -35,4 +43,79 @@ function validaQuizz(){
         alert("Precisa de um mínimo de 2 níveis")
         validaQtdePerguntas.value = ""
     }
+
+    quantidadeDePerguntas = validaQtdePerguntas;
+}
+
+function preencherAparece(icone){
+    contadorPergunta++;
+    const paiDoElemento = icone.parentElement;
+    const avoDoElemento = paiDoElemento.parentElement;
+    paiDoElemento.classList.add("none");
+    if((contadorPergunta) < quantidadeDePerguntas){
+    avoDoElemento.innerHTML += `
+    <div class="pergunta flexColumn">
+    <span class="estiloDaFonte">Pergunta ${contadorPergunta}</span>
+    <input type="text" placeholder="Texto da pergunta">
+    <input type="text" placeholder="Cor de fundo da pergunta">
+    </div>
+    
+    <div class="respostaCorreta flexColumn">
+    <span class="estiloDaFonte">Resposta correta</span>
+    <input type="text" placeholder="Resposta correta">
+    <input type="text" placeholder="URL da imagem">
+    </div>
+    
+    <div class="respostasIncorretas flexColumn">
+    <span class="estiloDaFonte">Respostas incorretas</span>
+    <input class="inputPerguntas" type="text" placeholder="Resposta incorreta 1">
+    <input class="inputPerguntas" type="text" placeholder="URL da imagem 1">
+    <input type="text" placeholder="Resposta incorreta 2">
+    <input type="text" placeholder="URL da imagem 2">
+    <input type="text" placeholder="Respsota incorreta 3">
+    <input type="text" placeholder="URL da imagem 3">
+    </div>
+
+    <div class="flex containerPergunta"> 
+    <span class="estiloDaFonte">Pergunta ${contadorPergunta + 1}</span>
+    <ion-icon name="create-outline" onclick ="preencherAparece(this)"></ion-icon>
+    </div>    `
+    }else if(contadorPergunta===quantidadeDePerguntas){
+        avoDoElemento.innerHTML += `
+        <div class="pergunta flexColumn">
+        <span class="estiloDaFonte">Pergunta ${contadorPergunta}</span>
+        <input type="text" placeholder="Texto da pergunta">
+        <input type="text" placeholder="Cor de fundo da pergunta">
+        </div>
+        
+        <div class="respostaCorreta flexColumn">
+        <span class="estiloDaFonte">Resposta correta</span>
+        <input type="text" placeholder="Resposta correta">
+        <input type="text" placeholder="URL da imagem">
+        </div>
+        
+        <div class="respostasIncorretas flexColumn">
+        <span class="estiloDaFonte">Respostas incorretas</span>
+        <input class="inputPerguntas" type="text" placeholder="Resposta incorreta 1">
+        <input class="inputPerguntas" type="text" placeholder="URL da imagem 1">
+        <input type="text" placeholder="Resposta incorreta 2">
+        <input type="text" placeholder="URL da imagem 2">
+        <input type="text" placeholder="Respsota incorreta 3">
+        <input type="text" placeholder="URL da imagem 3">
+        </div>
+    
+        <div class="flex containerPergunta"> 
+        <span class="estiloDaFonte">Pergunta ${contadorPergunta + 1}</span>
+        <ion-icon name="create-outline" onclick ="preencherAparece(this)"></ion-icon>
+        </div>  
+        
+        <button class="prosseguir" onclick:"validaPerguntas()">Prosseguir pra criar níveis</button>
+        `
+    }else{
+        alert("Não é possível criar uma nova pergunta, limite escolhido atingido");
+    }
+}
+
+function validaPerguntas(){
+
 }
