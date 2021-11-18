2eb9db05c12f58bcb0752a50c414d9ee33c0c19f
Luís Henrique Braga de Sousa <agarb@pop-os.localdomain>
2021-11-18 12:24:38 -0300

    Criando a função de validação do Quizz

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function quizzesLocais(){
         seusQuizzesAparece.classList.remove("none");
     }
 }
+
+function validaQuizz(){
+    const validaTitulo = document.querySelector(".tituloQuizz").value
+    const validaQtdePerguntas = document.querySelector(".qtdePerguntasQuizz").value
+    const validaQtdeNiveis = document.querySelector(".qtdeNiveisQuizz").value
+    if(validaTitulo.length < 20 || validaTitulo.length > 65 || validaTitulo=== "" || validaTitulo === null){
+        alert("O título é inválido, deve conter de 20 a 65 caracteres");
+        validaTitulo.value = "";
+    }
+    if(validaQtdePerguntas < 3 || validaQtdePerguntas === "" || validaQtdePerguntas === null){
+        alert("Mínimo de perguntas é 3");
+        validaQtdePerguntas.value = ""
+    }
+    if(validaQtdeNiveis < 2 || validaQtdeNiveis === "" || validaQtdeNiveis === null){
+        alert("Precisa de um mínimo de 2 níveis")
+        validaQtdePerguntas.value = ""
+    }
+}
